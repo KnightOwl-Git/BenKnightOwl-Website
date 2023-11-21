e960483b1ed6acec37b4fde0f47d38898c54e73f
Knight Owl <benjaminknight@me.com>
2023-11-21 02:43:56 -0800

    let's try squoosh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 import { defineConfig } from 'astro/config'
 
+import { defineConfig, squooshImageService } from 'astro/config';
+
+
 export default defineConfig({
   site: 'https://benknightowl.com',
+  image: {
+    service: squooshImageService(),
+  },
 })
+
